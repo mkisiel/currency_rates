3eb8e87eb3af93d088d46b868663b06e9722373b
Michal Kisiel <michal@iterato.rs>
2018-11-22 14:54:18 +0100

    Removed DownloadDataTask. Java 1.8 compatibility.

--- KotlinCurrency/app/src/main/java/com/example/patrycja/kotlincurrency/DownloadDataTaskCallback.java
+++
@@ -1,9 +0,0 @@
-package com.example.patrycja.kotlincurrency;
-
-import com.example.patrycja.kotlincurrency.api.model.Rate;
-
-//komunikacja miedzy JobService i AsyncTask
-public interface DownloadDataTaskCallback {
-    void taskFinished(Rate rate);
-    void taskFailed();
-}
--- KotlinCurrency/app/src/main/java/com/example/patrycja/kotlincurrency/service/MyJobService.java
+++ KotlinCurrency/app/src/main/java/com/example/patrycja/kotlincurrency/service/MyJobService.java
@@ -1,63 +1,75 @@
 package com.example.patrycja.kotlincurrency.service;
 
 
+import android.annotation.SuppressLint;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.util.Log;
 
-import com.example.patrycja.kotlincurrency.DownloadDataTask;
-import com.example.patrycja.kotlincurrency.DownloadDataTaskCallback;
 import com.example.patrycja.kotlincurrency.NotificationHelper;
+import com.example.patrycja.kotlincurrency.api.RestClient;
 import com.example.patrycja.kotlincurrency.api.model.Rate;
+import com.example.patrycja.kotlincurrency.api.model.Table;
+
+import java.util.List;
+
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MyJobService extends JobService {
 
     private static final String TAG = MyJobService.class.getSimpleName();
     private static final double MAX_RATE = 3.9;
     private static final double MIN_RATE = 3.7;
-    private JobParameters params;
+    private static final String USD_CODE = "USD";
 
+    @SuppressLint("CheckResult")
     @Override
-    public boolean onStartJob(JobParameters params) {
+    public boolean onStartJob(final JobParameters params) {
         Log.d(TAG, "onStartJob: ");
-        new DownloadDataTask(callback).execute();
-        this.params = params;
+        RestClient.getInstance().getCurrencyService().getTableA()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(responseList -> {
+                    Log.d(TAG, "taskFinished: ");
+                    //sprawdzenie poziom mid
+                    //jeśli warunek spełniony - tworzenie notyfikacji
+                    //jeśli nie - finishJob
+                    if (responseList != null) {
+                        for (Table response : responseList) {
+                            for (Rate rate : response.getRates()) {
+                                if (rate.getCode().equals(USD_CODE)) {
+                                    if (rate.getMid() > MAX_RATE) {
+                                        Log.d(TAG, "taskFinished: nice currency ");
+                                        NotificationHelper notification = new NotificationHelper(getApplicationContext());
+                                        notification.createNotification("Warning", "The USD/PLN rate has exceeded the maximum limit");
+                                    } else if (rate.getMid() < MIN_RATE) {
+                                        Log.d(TAG, "taskFinished: nice currency ");
+                                        NotificationHelper notification = new NotificationHelper(getApplicationContext());
+                                        notification.createNotification("Warning", "The USD/PLN rate has exceeded the minimum limit");
+                                    }
+
+                                    jobFinished(params, false);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                    jobFinished(params, true);
+
+                }, throwable -> {
+                    Log.d(TAG, "taskFailed: ");
+                    jobFinished(params, true);
+                });
         return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
         Log.d(TAG, "onStopJob: ");
-        this.params = null;
         return false;
     }
 
-    DownloadDataTaskCallback callback = new DownloadDataTaskCallback() {
-        @Override
-        public void taskFinished(Rate rate) {
-            Log.d(TAG, "taskFinished: ");
-            //sprawdzenie poziom mid
-            //jeśli warunek spełniony - tworzenie notyfikacji
-            //jeśli nie - finishJob
-
-            if(rate.getMid()>MAX_RATE){
-                Log.d(TAG, "taskFinished: nice currency ");
-                NotificationHelper notification = new NotificationHelper(getApplicationContext());
-                notification.createNotification("Warning", "The USD limit has exceeded the maximum limit");
-            } else if (rate.getMid()<MIN_RATE){
-                Log.d(TAG, "taskFinished: nice currency ");
-                NotificationHelper notification = new NotificationHelper(getApplicationContext());
-                notification.createNotification("Warning", "The USD limit has exceeded the minimum limit");
-            }
-
-            jobFinished(params, false);
-        }
-
-        @Override
-        public void taskFailed() {
-            Log.d(TAG, "taskFailed: ");
-            jobFinished(params, true);
-        }
-    };
 }
 
